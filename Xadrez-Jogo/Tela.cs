81f34bfd14f6dc53f8cab38d8e2fc88f2b0ef9c9
diegolins10 <diegofernandeslins@gmail.com>
2021-07-21 20:49:32 -0300

    criacao clas tela para mostrar na tela o tabuleiro

---
+++ Xadrez-Jogo/Tela.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xadrez_Jogo;
+
+namespace Xadrez_Jogo.Tabuleiro
+{
+    class Tela
+    {
+        /*
+         * Metodo estatico para imprimir visualmente a matriz na tela 
+         * para representar o tabuleiro
+         */
+        public static void ImprimirTabuleiro(Tabuleiro tab)
+        {
+            //linhas
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                //colunas
+                for (int j = 0; j < tab.Colunas; j++)
+                {
+                    if (tab.peca(i, j) == null) // verificando se a posicao do tabuleiro está vazia
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
