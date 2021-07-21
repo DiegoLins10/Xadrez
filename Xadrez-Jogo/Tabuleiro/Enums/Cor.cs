ffed0e6cf71e32bbf0b667138cb543851055b2fb
diegolins10 <diegofernandeslins@gmail.com>
2021-07-21 20:22:26 -0300

    Enums

---
+++ Xadrez-Jogo/Tabuleiro/Enums/Cor.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Xadrez_Jogo.Tabuleiro.Enums
+{/*
+  * Aqui é onde fica a enumeracao do nosso projeto que vai representar as cores das pecas
+  */
+    enum Cor
+    {
+        Branca = 0,
+        Preta = 1,
+        Amarela = 2,
+        Azul = 3,
+        Vermelha = 4,
+        Verde = 5,
+        Laranja = 6
+    }
+}
