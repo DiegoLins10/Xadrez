25eca17930c856191ba7923cbaea3e90cee33681
diegolins10 <diegofernandeslins@gmail.com>
2021-07-21 20:23:03 -0300

    criacao tabuleiro

---
+++ Xadrez-Jogo/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Xadrez_Jogo.Tabuleiro
+{
+    /*
+     * Classe que vai representar o objeto tabuleiro 
+     */
+    class Tabuleiro
+    {
+      /*
+      * Properties e atributos, usando um atributo matriz do tipo Peca para representar o tabuleiro
+      */
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas; 
+
+        /*
+         * Construtores
+         */
+        public Tabuleiro()
+        {
+
+        }
+
+        public Tabuleiro(int linhas, int colunas, Peca[,] pecas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            this.pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
