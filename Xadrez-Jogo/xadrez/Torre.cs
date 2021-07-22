0fe94f5c4426f698d6a861fa4ab48f8ac3c1e141
diegolins10 <diegofernandeslins@gmail.com>
2021-07-21 21:19:14 -0300

    sub class torre

---
+++ Xadrez-Jogo/xadrez/Torre.cs
@@ -0,0 +1,18 @@
+﻿using Xadrez_Jogo.tabuleiro;
+using Xadrez_Jogo.tabuleiro.Enums;
+
+namespace Xadrez_Jogo.xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
