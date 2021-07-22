16ad9a7cbd73921927be451801869f683d76dbac
diegolins10 <diegofernandeslins@gmail.com>
2021-07-21 21:19:06 -0300

    sub class rei

---
+++ Xadrez-Jogo/xadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿using Xadrez_Jogo.tabuleiro;
+using Xadrez_Jogo.tabuleiro.Enums;
+
+namespace Xadrez_Jogo.xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
