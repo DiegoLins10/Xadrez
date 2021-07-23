636806a82a5133852cbfdf7fe799c2185f585830
diegolins10 <diegofernandeslins@gmail.com>
2021-07-23 18:57:44 -0300

    exceptions personalizadas

---
+++ Xadrez-Jogo/tabuleiro/Exceptions/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez_Jogo.tabuleiro.Exceptions
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string message) : base(message)
+        {
+
+        }
+    }
+}
