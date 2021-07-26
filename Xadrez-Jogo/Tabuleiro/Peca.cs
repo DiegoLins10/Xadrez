35a741f88d4249da9c0287a2d8ea0e77f26f8963
Luffy <diegofernandeslins@gmail.com>
2021-07-26 13:49:05 -0300

    criação metodo abstract na classe pai

--- Xadrez-Jogo/Tabuleiro/Peca.cs
+++ Xadrez-Jogo/Tabuleiro/Peca.cs
@@ -3,13 +3,14 @@
 namespace Xadrez_Jogo.tabuleiro
 {   /*
      * Nessa classe é onde sera registrado o objeto peca do jogo
+     * a classe tem um metodo abstrato, ou seja ela tem que ser abstrata.
      */
-    class Peca
+    abstract class Peca
     {
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
         public int QteMovimentos { get; protected set; }
-        public Tabuleiro Tabuleiro { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
 
         /*
          * Construtores
@@ -24,12 +25,23 @@ namespace Xadrez_Jogo.tabuleiro
             Posicao = null;
             Cor = cor;
             QteMovimentos = 0;
-            Tabuleiro = tabuleiro;
+            Tab = tabuleiro;
         }
 
+        /*
+         * Incrementa quantidade de movimentos
+         */
         public void IncrementarQteMovimentos()
         {
             QteMovimentos++;
         }
+
+        /*
+         * Metodo abstrato da super classe
+         * pois cada peça é generica e tem regras diferentes.
+         * deve ser implementada na classe filha
+         */
+        public abstract bool[,] movimentosPosiveis();
+        
     }
 }
