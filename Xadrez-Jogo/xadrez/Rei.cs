46af6ecb90200ea6e3f2a2cb108a3605c53bb07f
Luffy <diegofernandeslins@gmail.com>
2021-07-26 13:48:34 -0300

    Implementação regra do rei

--- Xadrez-Jogo/xadrez/Rei.cs
+++ Xadrez-Jogo/xadrez/Rei.cs
@@ -5,14 +5,91 @@ namespace Xadrez_Jogo.xadrez
 {
     class Rei : Peca
     {
+        /*
+         * Construtores
+         */
         public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
         {
 
         }
 
+        /*
+         * ToString da classe
+         */
         public override string ToString()
         {
             return "R";
+        }
+
+        /*
+         * Verifica se a pesa pdoe mover para a posição
+         * conforme as regras do xadrez
+         */
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != this.Cor;
+        }
+
+        /*
+         * sobrescrevendo metodo abstract da classe pai
+         * aqui é colocada a regra de movimentãção do rei
+         * esse metodo está atribuindo as regras para onde o rei pode se mexer.
+         */
+        public override bool[,] movimentosPosiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //nordeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //sudeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if(Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //sudoeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            // noroeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
         }
     }
 }
